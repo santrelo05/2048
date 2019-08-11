4f61046563b3cd90366b5d8fe152388ae0186fac
santiago restrepo <santiago.restrepol@upb.edu.co>
2019-08-10 21:16:16 -0500

    funcionalidad up

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let botones = Array.from(document.querySelectorAll(".boton"));
 let cont = 0;
 let fila = [1,2,3,4];
 let colum = [4,8,12,16];
-let pila = [],blank=0;
+let pila = [],past=0,now=-1,global = 0;
 window.onload = init;
 
 function init(){
@@ -31,45 +31,37 @@ function setElement(){
 }
 
 function moveUp(){
-    var global = 3;
     for(var i=0;i<4;i++){
-        if(botones[i+(4*global)].value!=" "){
-            pila.push(parseInt(botones[i+(4*global)].value));
-            botones[i+(4*global)].value =" ";
-        }
-        if(global==0){
-        global = 4;
-            if(pila.length>1){
-                sumarUp(i);
+        for(var j=0;j<4;j++){
+            if(botones[i+(4*j)].value!=" "){
+                pila.push(parseInt(botones[i+(4*j)].value));
+                botones[i+(4*j)].value = " ";
             }
-            else{
-                let x = pila.pop();
-                if(x===undefined){
-                    botones[i].value = " ";  
-                }
-                else{
-                    botones[i].value = x; 
-                }
-            }
-        }else{
-            i--;
         }
-        global--;
+        sumarUp(i);
     }
+    setElement();
 }
 function sumarUp(target){
-    let longitud = pila.length;
-    bank=pila.pop();
-    
-        for(var i=0;i<longitud-1;i++){
-            let now = pila.pop();
-            if(blank === now){
-                botones[target].value= blank+now;
-                blank = 0;
+    var longitud = pila.length;
+    var posicion = 0;
+    if(longitud>1){
+        for(var i = 0; i<pila.length;i++){
+            if(pila[i] == pila[i+1]){
+                botones[target+(posicion*4)].value = pila[i] + pila[i+1];        
+                posicion++;
+                i++;
             }
             else{
-                botones[target].value = blank;
-                blank = now;
+                botones[target+(posicion*4)].value = pila[i];
+                posicion++;
             }
         }
+        
+    }
+    else if(longitud==1){
+        botones[target].value = pila.pop();
+       
+    }
+    pila=[];
 }
