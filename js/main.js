5f6800f5d2870cb763ba09a5341e0574c09c8baf
santiago restrepo <santiago.restrepol@upb.edu.co>
2019-08-14 10:42:05 -0500

    suma de imagenes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ mc.on("swipeleft", function () {
 }); 
 
 mc.on("swiperight", function () { 
- 
+ alert("derecha");
     moveRight();
 });
 
@@ -23,6 +23,139 @@ mc.on("swipedown", function () {
     moveDown();
 });
 
+
+let botones = Array.from(document.querySelectorAll("img"));
+var nameclass,suma;
+let cont = 0;
+let pila = [];
+var posicion,extra;
+window.onload = init;
+
+function init(){
+setElement();
+setElement();
+
+}
+
+function setElement(){
+    while(cont < 1){
+        let numero = Math.floor(Math.random() * 16) + 1;
+        let valor = parseInt(botones[numero-1].classList[0]);
+        if(valor === 0){
+            let probab = Math.floor(Math.random() * 10) + 1;
+            if(probab<10){
+            botones[numero-1].src="../img/2.png";
+            botones[numero-1].classList.remove("0");
+            botones[numero-1].classList.add("2");
+            }else{
+                botones[numero-1].src="../img/4.png";
+                botones[numero-1].classList.remove("0");
+                botones[numero-1].classList.add("4");
+            }
+            cont=1;
+        }
+    }
+    cont = 0;
+}
+
+function moveRight(){
+    for(var i=0;i<4;i++){
+        for(var j=3;j>=0;j--){
+            if(botones[j+(4*i)].classList[0]!="0"){
+                pila.push(parseInt(botones[j+(4*i)].classList[0]));
+                nameclass = botones[j+(4*i)].classList[0];
+                botones[j+(4*i)].classList.remove(nameclass);
+                botones[j+(4*i)].classList.add("0");
+                botones[j+(4*i)].src = "../img/logo1.jpg"
+            } 
+        }
+        sumar(i,3,-1,4,1);
+    }
+    setElement();
+}
+function moveLeft(){
+    for(var i=0;i<4;i++){
+        for(var j=0;j<4;j++){
+            if(botones[j+(4*i)].classList[0]!="0"){
+                pila.push(parseInt(botones[j+(4*i)].classList[0]));
+                nameclass = botones[j+(4*i)].classList[0];
+                botones[j+(4*i)].classList.remove(nameclass);
+                botones[j+(4*i)].classList.add("0");
+                botones[j+(4*i)].src = "../img/logo1.jpg"
+            } 
+        }
+        sumar(i,0,1,4,1);
+    }
+    setElement();
+}
+
+function moveDown(){
+    for(var i=0;i<4;i++){
+        for(var j=3;j>=0;j--){
+            if(botones[i+(4*j)].classList!="0"){
+                pila.push(parseInt(botones[i+(4*j)].classList[0]));
+                nameclass = botones[i+(4*j)].classList[0];
+                botones[i+(4*j)].classList.remove(nameclass);
+                botones[i+(4*j)].classList.add("0");
+                botones[i+(4*j)].src = "../img/logo1.jpg";
+            }
+        }
+        sumar(i,3,-1,1,4);
+    }
+    setElement();
+}
+
+function moveUp(){
+    for(var i=0;i<4;i++){
+        for(var j=0;j<4;j++){
+            if(botones[i+(4*j)].classList!="0"){
+                pila.push(parseInt(botones[i+(4*j)].classList[0]));
+                nameclass = botones[i+(4*j)].classList[0];
+                botones[i+(4*j)].classList.remove(nameclass);
+                botones[i+(4*j)].classList.add("0");
+                botones[i+(4*j)].src = "../img/logo1.jpg";
+            }
+        }
+        sumar(i,0,1,1,4);
+    }
+    setElement();
+}
+
+function sumar(target,posicion,extra,multi1,multi2){
+    var longitud = pila.length;
+    if(longitud>1){
+        for(var i = 0; i<pila.length;i++){
+            if(pila[i] == pila[i+1]){
+                suma = pila[i] + pila[i+1];
+                nameclass = botones[(target*multi1)+(posicion*multi2)].classList[0];
+                botones[(target*multi1)+(posicion*multi2)].classList.remove(nameclass);
+                botones[(target*multi1)+(posicion*multi2)].classList.add(suma);
+                botones[(target*multi1)+(posicion*multi2)].src= "../img/"+suma+".png";     
+                posicion= posicion + extra;
+                i++;
+            }
+            else{
+                nameclass = botones[(target*multi1)+(posicion*multi2)].classList[0];
+                botones[(target*multi1)+(posicion*multi2)].classList.remove(nameclass);
+                botones[(target*multi1)+(posicion*multi2)].classList.add(pila[i]);
+                botones[(target*multi1)+(posicion*multi2)].src= "../img/"+pila[i]+".png";
+                posicion = posicion + extra;
+            }
+        }
+        
+    }
+    else if(longitud==1){
+
+        suma = pila.pop();
+        nameclass = botones[(target*multi1)+(posicion*multi2)].classList[0];
+        botones[(target*multi1)+(posicion*multi2)].classList.remove(nameclass);
+        botones[(target*multi1)+(posicion*multi2)].classList.add(suma);
+        botones[(target*multi1)+(posicion*multi2)].src = "../img/"+suma+".png";
+    }
+    pila=[];
+}
+
+/*
 let botones = Array.from(document.querySelectorAll(".boton"));
 let cont = 0;
 let fila = [1,2,3,4];
@@ -135,3 +268,4 @@ function sumar(target,posicion,extra,multi1,multi2){
     }
     pila=[];
 }
+*/
