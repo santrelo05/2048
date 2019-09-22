210bf7d6221f49f0278476e8aed4f8e88ac44920
santiago restrepo <santiago.restrepol@upb.edu.co>
2019-09-21 19:04:03 -0500

    sistema record

--- js/main.js
+++ js/main.js
@@ -2,35 +2,44 @@ var nameclass,suma;
 let cont = 0;
 let pila = [];
 var posicion,extra;
-var contayuda;
 var pila1=[];
-var tablero=[[0],[],[],[]]
-var contboton=0;
 var igual = 0;
 var verf= 0;
 var verfwin = 0;
 var puntaje=0;
-var record=[0];
+var record=[0,0,0,0,0,0,0,0,0,0];
 var mxrecord=0;
 let botones;
 var element;
 var Record;
 var Puntaje;
+var NombreUsuario;
+var recname=[];
+var recpoint=[];
+var recmundial=[];
 
 window.onload = init;
 
 function init(){
+    var firebaseConfig = {
+    apiKey: "AIzaSyA4-vQKzh6DY8aRkkvOrwFVauk1TsKv5cQ",
+    authDomain: "roadto2048.firebaseapp.com",
+    databaseURL: "https://roadto2048.firebaseio.com",
+    projectId: "roadto2048",
+    storageBucket: "roadto2048.appspot.com",
+    messagingSenderId: "863459540195",
+    appId: "1:863459540195:web:fc39aaef51c0e76e9019d6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
     botones = Array.from(document.querySelectorAll(".caja"));
+    recname = Array.from(document.querySelectorAll(".nameRec"));
+    recpoint = Array.from(document.querySelectorAll(".pointRec"));
     Puntaje = document.getElementById("Puntaje");
     element = document.getElementById("cuadro");
     Record =  document.getElementById("Record");
-
-    for(i in record){
-        if(record[i]>mxrecord){
-            mxrecord=record[i];
-        }
-    }
-    Record.innerHTML = "Record: "+mxrecord;
+       
     for(i in botones){
         botones[i].classList.remove("caja");
         botones[i].classList.add("0");
@@ -44,12 +53,12 @@ setTimeout(hideLoader, 2 * 1000);
 
 function hideLoader() {
     document.getElementById("loading").classList.add("none");
-    document.getElementById("pantinicial").classList.remove("none");
+    document.getElementById("Login").classList.remove("none");
 }
 
 function setElement(){
    
-    contboton=0;
+   
     verfwin = verificarwin();
     verf = verificar();
 
@@ -79,12 +88,88 @@ function setElement(){
             }
         }
         else{
+          
+
             record.push(puntaje);
+            record.sort((a,b)=>b-a);
+            record.pop();
+            
+
+            var ref = firebase.database().ref("Usuarios/"+NombreUsuario+"/record");
+            ref.update({
+                v0 : record[0],
+                v1 : record[1],
+                v2 : record[2],
+                v3 : record[3],
+                v4 : record[4],
+                v5 : record[5],
+                v6 : record[6],
+                v7 : record[7],
+                v8 : record[8],
+                v9 : record[9],
+            });
+            var obj = {
+                "nombre" : NombreUsuario,
+                "puntaje": puntaje
+            };
+            
+            recmundial.push(obj);
+            recmundial.sort((a,b)=>b.puntaje-a.puntaje);
+            recmundial.pop();
+            for(i in recmundial){
+                var ref = firebase.database().ref("Record/v"+i);
+                ref.update({
+                  "nombre" : recmundial[i].nombre,
+                  "puntaje" : recmundial[i].puntaje
+                });
+            }
+
+
+            puntaje = 0;
+            Record.innerHTML = "Record: "+record[0];
+            Puntaje.innerHTML="Puntaje:<br>"+puntaje;
+            topcol();
             cambiodenone('juego','perdiste');
         }
     }
     else{
         record.push(puntaje);
+        record.sort((a,b)=>b-a);
+        record.pop();
+
+        var ref = firebase.database().ref("Usuarios/"+NombreUsuario+"/record");
+        ref.update({
+            v0 : record[0],
+            v1 : record[1],
+            v2 : record[2],
+            v3 : record[3],
+            v4 : record[4],
+            v5 : record[5],
+            v6 : record[6],
+            v7 : record[7],
+            v8 : record[8],
+            v9 : record[9],
+        });
+        var obj = {
+            "nombre" : NombreUsuario,
+            "puntaje": puntaje
+        };
+        
+        recmundial.push(obj);
+        recmundial.sort((a,b)=>b.puntaje-a.puntaje);
+        recmundial.pop();
+        for(i in recmundial){
+            var ref = firebase.database().ref("Record/v"+i);
+            ref.update({
+              "nombre" : recmundial[i].nombre,
+              "puntaje" : recmundial[i].puntaje
+            });
+        }
+
+        puntaje = 0;
+        Record.innerHTML = "Record: "+record[0];
+        Puntaje.innerHTML="Puntaje:<br>"+puntaje;
+        topcol();
         cambiodenone('juego','ganaste');
     } 
     igual = 0;
@@ -259,16 +344,58 @@ function verificarwin(){
 
 function reload(pantalla1,pantalla2){
     record.push(puntaje);
-    puntaje = 0;
-    for(i in record){
-        if(record[i]>mxrecord){
-            mxrecord=record[i];
-        }
+    record.sort((a,b)=>b-a);
+    record.pop();
+  
+
+    var ref = firebase.database().ref("Usuarios/"+NombreUsuario+"/record");
+    ref.update({
+        v0 : record[0],
+        v1 : record[1],
+        v2 : record[2],
+        v3 : record[3],
+        v4 : record[4],
+        v5 : record[5],
+        v6 : record[6],
+        v7 : record[7],
+        v8 : record[8],
+        v9 : record[9],
+    });
+    var obj = {
+        "nombre" : NombreUsuario,
+        "puntaje": puntaje
+    };
+    
+    recmundial.push(obj);
+    recmundial.sort((a,b)=>b.puntaje-a.puntaje);
+    recmundial.pop();
+    for(i in recmundial){
+        var ref = firebase.database().ref("Record/v"+i);
+        ref.update({
+          "nombre" : recmundial[i].nombre,
+          "puntaje" : recmundial[i].puntaje
+        });
     }
-    Record.innerHTML = "Record: "+mxrecord;
+
+
+    puntaje = 0;
+
+
+    Record.innerHTML = "Record: "+record[0];
     Puntaje.innerHTML="Puntaje:<br>"+puntaje;
     cambiodenone(pantalla1,pantalla2);
-    nameclass
+    for(i in botones){
+        nameclass = botones[i].classList[0];
+        botones[i].classList.remove(nameclass);
+        botones[i].classList.add("0");
+        botones[i].src= "img/0.png";  
+    }
+    setElement();
+    setElement();
+}
+function reload2(pantalla1,pantalla2){
+
+    cambiodenone(pantalla1,pantalla2);
     for(i in botones){
         nameclass = botones[i].classList[0];
         botones[i].classList.remove(nameclass);
@@ -305,4 +432,147 @@ mc.on("swipedown", function () {
 });
 
 cambiodenone('pantinicial','juego');
+}
+
+function push(){
+    var ref = firebase.database().ref('Usuarios/santi');
+    var newPostRef = ref.push();
+    newPostRef.set({
+        contra: contra,
+            record:{
+            v0 : 0,
+            v1 : 0,
+            v2 : 0,
+            v3 : 0,
+            v4 : 0,
+            v5 : 0,
+            v6 : 0,
+            v7 : 0,
+            v8 : 0,
+            v9 : 0,
+            }
+    });
+}
+
+function Registro(){
+    var name = document.getElementById("username").value;
+    var contra = document.getElementById("contra").value; 
+    var ref = firebase.database().ref("Usuarios");
+        ref.once("value")
+        .then(function(snapshot) {
+    var hasname = snapshot.child(name).exists();
+    if(hasname === true){
+        alert("nombre en uso");
+    }
+    else{
+        ref.child(name).set({
+            contra: contra,
+            record:{
+            v0 : 0,
+            v1 : 0,
+            v2 : 0,
+            v3 : 0,
+            v4 : 0,
+            v5 : 0,
+            v6 : 0,
+            v7 : 0,
+            v8 : 0,
+            v9 : 0,
+            }
+        });
+        for(var i = 0 ; i<10; i++ ){
+            var ref1 = firebase.database().ref("Record/v"+i);
+            ref1.once("value")
+            .then(function(snapshot) {
+                var obj = {
+                    "nombre" : snapshot.child("nombre").val(),
+                    "puntaje": snapshot.child("puntaje").val()
+                };
+                recmundial.push(obj);
+            });
+        }
+     
+        Record.innerHTML = "Record: 0";
+        NombreUsuario=name;
+        cambiodenone('Registro','pantinicial');
+    }
+  });
+ 
+}
+function Login(){
+    var name = document.getElementById("usernameL").value;
+    var contra = document.getElementById("contraL").value;
+    var ref = firebase.database().ref("Usuarios");
+        ref.once("value")
+        .then(function(snapshot) {
+    var hasname = snapshot.child(name).exists();
+    if(hasname===true){
+        var pws = snapshot.child(name+'/contra').val();
+        if(pws===contra){
+            NombreUsuario = name;
+            cambiodenone('Login','pantinicial');
+         
+            var ref = firebase.database().ref("Usuarios/"+NombreUsuario+"/record");
+            ref.once("value")
+            .then(function(snapshot) {
+                for(i in record){
+                    record[i] = snapshot.child("v"+i).val();
+                }
+                record.sort((a,b)=>b-a);
+           
+            Record.innerHTML = "Record: "+record[0];
+            });
+            for(var i = 0 ; i<10; i++ ){
+                var ref = firebase.database().ref("Record/v"+i);
+                ref.once("value")
+                .then(function(snapshot) {
+                    var obj = {
+                        "nombre" : snapshot.child("nombre").val(),
+                        "puntaje": snapshot.child("puntaje").val()
+                    };
+                    recmundial.push(obj);
+                });
+            }
+            
+        }
+        else{
+            alert("contraseña incorrecta");
+        }
+    }
+    else{
+        alert("usuario incorrecto");
+    }     
+    });
+}
+
+function topcol(){
+    var r1 = document.getElementById("recordtit1");
+    var r2 = document.getElementById("recordtit2");
+
+    r1.classList.remove("rr1");
+    r2.classList.remove("rr2");
+    for(i in recname){
+        recname[i].innerHTML = recmundial[i].nombre;
+        recpoint[i].innerHTML = recmundial[i].puntaje;
+    }
+}
+
+function mitop(){
+    var r1 = document.getElementById("recordtit1");
+    var r2 = document.getElementById("recordtit2");
+
+    r1.classList.add("rr1");
+    r2.classList.add("rr2");
+
+    for(i in recname){
+        recname[i].innerHTML = NombreUsuario;
+        recpoint[i].innerHTML = record[i];
+    }
+
+
+
+
+
+
+
 }
